abc31e680bffa9d78950d9309cafbd4aaccd62b5
luk3yx <luk3yx@users.noreply.github.com>
2018-10-20 13:18:29 +1300

    Fix handling of longer messages

--- miniirc.py
+++ miniirc.py
@@ -53,7 +53,7 @@ class IRC:
                 self.sendq = None
                 for i in sendq:
                     self.quote(*i)
-            msg = '{}\r\n'.format(' '.join(msg)).encode('utf-8')[:512]
+            msg = '{}'.format(' '.join(msg)).encode('utf-8')[:510] + b'\r\n'
             self.sock.send(msg)
         else:
             self.debug('>Q>', *msg)
