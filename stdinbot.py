4056af76045677daeb2c8a52cb0cc72ae77e68e4
luk3yx <luk3yx@users.noreply.github.com>
2018-10-27 18:00:01 +1300

    Use irc.disconnect() instead of irc.quote() in stdinbot

--- stdinbot.py
+++ stdinbot.py
@@ -31,14 +31,14 @@ irc = IRC(ip, port, nick, channels, ident = ident, realname = realname,
 # Read stdin
 @irc.Handler('001')
 def handle_stdin(irc, hostmask, args):
-    qmsg = 'QUIT :I reached the end of my file, therefore my life™.'
+    qmsg = 'I reached the end of my file, therefore my life™.'
     while True:
         try:
             line = '\u200b' + input().replace('\r', '').replace('\n', '  ')
         except:
             line = '\x04'
         if line == '\x04':
-            irc.quote(qmsg)
+            irc.disconnect(qmsg)
             return os._exit(0)
         irc.msg(channels[0], line)
 
