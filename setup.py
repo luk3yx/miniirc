59bb6f65a767ff58fdc6eacb2b257671c3dd5e1d
luk3yx <luk3yx@users.noreply.github.com>
2018-10-28 08:57:39 +1300

    Split miniirc into a separate repo

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    desc = f.read()
+
+setup(
+    name        = 'miniirc',
+    version     = '0.2.3',
+    py_modules  = ['miniirc'],
+    author      = 'luk3yx',
+    description = 'A lightweight IRC framework.',
+    license     = 'MIT',
+
+    long_description              = desc,
+    long_description_content_type = 'text/markdown',
+    install_requires              = ['certifi'],
+
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+    ]
+)
