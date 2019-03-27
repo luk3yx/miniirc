7ae458a262f144f38aceaaca693d967653e6da45
luk3yx <luk3yx@users.noreply.github.com>
2019-03-27 14:53:11 +1300

    Bump PyPI version number to 1.1.3.post3  • This stops PyPI thinking the Python2 version is the latest.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name        = 'miniirc',
-    version     = '1.1.3',
+    version     = '1.1.3.post3',
     py_modules  = ['miniirc'],
     author      = 'luk3yx',
     description = 'A lightweight IRC framework.',
